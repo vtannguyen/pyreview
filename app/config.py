1c946e923bb9946eae24418ae5fbb219f8ef6f17
Tan Nguyen <tantit91@gmail.com>
2024-06-14 16:58:06 +0200

    add some comments to configuration

--- app/config.py
+++ app/config.py
@@ -2,11 +2,11 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
-    TARGET_PROJECT: str | None = None
+    TARGET_PROJECT: str | None = None  # absolute path to target project root
     TARGET_BRANCH: str = "master"
-    CODE_DIR: str | None = None
+    CODE_DIR: str | None = None  # path to code directory relative to project root
     ACCEPTED_COMMENTS: list[str] = ["# Arrange", "# Act", "# Assert"]
-    RESULT_FILE_NAME: str = "comments.txt"
+    RESULT_FILE_NAME: str = "pyreview_report.txt"
     TEST_SETUP_COMMAND: str | None = None
     TEST_TEARDOWN_COMMAND: str | None = None
     COV_JSON_FILE_PATH: str = "cov.json"
