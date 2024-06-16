7398a9ae7bb9eb50b71ee76dcd7ebdf48e479da6
Tan Nguyen <tantit91@gmail.com>
2024-06-16 16:45:31 +0200

    fix logger in case target project is None

--- app/logger.py
+++ app/logger.py
@@ -7,9 +7,12 @@ logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
 terminal_handler = logging.StreamHandler()
-file_handler = logging.FileHandler(
-    os.path.join(settings.TARGET_PROJECT, settings.RESULT_FILE_NAME), mode="w"
+log_file_path = (
+    os.path.join(settings.TARGET_PROJECT, settings.RESULT_FILE_NAME)
+    if settings.TARGET_PROJECT
+    else settings.RESULT_FILE_NAME
 )
+file_handler = logging.FileHandler(log_file_path, mode="w")
 
 logger.addHandler(terminal_handler)
 logger.addHandler(file_handler)
